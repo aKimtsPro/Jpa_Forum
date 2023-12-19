3cf983534e792906925fea8167cac76d4f2fb98a
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-12-19 14:53:50 +0100

    demo jpa - basic entity

--- src/main/java/be/tftic/java/forum/entity/Post.java
+++ src/main/java/be/tftic/java/forum/entity/Post.java
@@ -1,14 +1,40 @@
 package be.tftic.java.forum.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
     private Long id;
+    @Column(name= "post_title", nullable = false)
     private String title;
+    @Column(name = "post_content", nullable = false)
     private String content;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
